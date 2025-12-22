95c1f52a82bc25c6afd76e90dbe2a2e8b3a1eaf2
oksamar <samarjeet025@gmail.com>
2025-12-22 22:26:47 +0530

    Tavily Package

---
+++ Tavily_Code_2.py
@@ -0,0 +1,21 @@
+# Import the API Keys from ".env" file
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch
+
+llm = ChatOpenAI()
+tools = [TavilySearch]
+agent = create_agent(model=llm, tools=tools)
+
+def main():
+    print("Hello from langchain-course!!!")
+    result = agent.invoke({"messages": [HumanMessage(content="What is the weather in Tokyo ?")]})
+    print(result)
+
+if __name__ == "__main__":
+    main()
